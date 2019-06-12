8a9d826e8517aec4fb4b24ce62e41b6a1b0c1d65
Billy Rotich <rotichbil636@gmail.com>
2019-06-13 00:29:02 +0200

    Find top five customers

--- customer.py
+++ customer.py
@@ -3,6 +3,19 @@ import csv
 
 unused_barcodes = 0
 
+
+def find_key(item_dict, item_val):
+    """
+    Returns the last key found in a dictionary
+    given a some value
+    """
+    result = None
+    for key, val in item_dict.items():
+        if val == item_val:
+            result = key
+    return result
+
+
 # create a dictionary to hold sorted grouped orders
 order_set = dict()
 # read data from barcodes file
@@ -49,6 +62,9 @@ with open('orders.csv') as order_csv:
         else:
             customer_set[customer_id] = [current_order]
 
+# Create top customers dictionary
+top_customers = dict()
+
 # generate output csv file
 with open('results.csv', 'w') as results_csv:
     fieldnames = ['customer_id', 'order_id', 'barcodes']
@@ -57,12 +73,37 @@ with open('results.csv', 'w') as results_csv:
 
     for customer_id in customer_set:
         orders = customer_set[customer_id]
+        ticket_count = 0
+
         for order in orders:
+            # Get order_ids and barcodes from the order dictionary
+            order_id = list(order.keys())[0]
+            barcode_list = [int(x) for x in list(order.values())[0]]
+
+            # Write line by line to the CSV file
             writer.writerow({
                 'customer_id': customer_id,
-                'order_id': list(order.keys())[0],
-                'barcodes': [int(x) for x in list(order.values())[0]]
+                'order_id': order_id,
+                'barcodes': barcode_list
             })
+
+            # Increment ticket counter
+            ticket_count += len(barcode_list)
+
+        if len(top_customers) >= 5:
+            min_tickets = min(top_customers.values())
+            if ticket_count > min_tickets:
+                to_remove = find_key(top_customers, min_tickets)
+                top_customers.pop(to_remove)
+                top_customers[customer_id] = ticket_count
+        else:
+            top_customers[customer_id] = ticket_count
+
+# print top customers
+print("\nThe top 5 customers are: ")
+for customer_id, tickets in sorted(top_customers.items()):
+    print("Customer ID: {} - Tickets bought: {}".format(customer_id, tickets))
+print("")  # new line
 
 # print unused barcodes
 print("Unused barcodes: {}".format(unused_barcodes))
